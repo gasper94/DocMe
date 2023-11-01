c78886fcabf109c8d03167f6b6c3166fe506ae43
gasper94 <ulimarmol94@gmail.com>
2023-11-01 16:45:03 -0700

    working on layout

--- packages/app/design/view.tsx
+++ packages/app/design/view.tsx
@@ -1,5 +1,10 @@
-import { View as ReactNativeView, Button as ReactNativeButton } from 'react-native'
+import {
+  View as ReactNativeView,
+  Button as ReactNativeButton,
+  ScrollView as ReactNativeScrollView,
+} from 'react-native'
 import { styled } from 'nativewind'
 
 export const View = styled(ReactNativeView)
 export const Button = styled(ReactNativeButton)
+export const ScrollView = styled(ReactNativeScrollView)
