8a0c0895e618d39a057ac35d6b3fe0974a8a4d15
gasper94 <ulimarmol94@gmail.com>
2023-11-16 12:06:10 -0800

    modifying adding an activyt button in navigation nav bar

--- packages/app/features/components/NavigationBar/NavigationBar.tsx
+++ packages/app/features/components/NavigationBar/NavigationBar.tsx
@@ -5,13 +5,15 @@ import { TextLink } from 'solito/link'
 import { Text } from 'app/design/typography'
 import { View } from 'app/design/view'
 import { MotiLink } from 'solito/moti'
+import { TouchableOpacity } from 'react-native'
+
+// Assets
+import Plus from '../../../../assets/Icons/plus/plus'
 
 export function NavigationScreen() {
   return (
     <SafeAreaView className="fixed left-0 right-0 top-0 mb-2 flex-1">
       <View className="flex-row items-center justify-between bg-transparent px-4">
-        {/* <Text>Image</Text> */}
-
         <MotiLink
           href="/"
           animate={({ hovered, pressed }) => {
@@ -38,31 +40,43 @@ export function NavigationScreen() {
           <Text className="color-white">DocuMe</Text>
         </View>
 
-        <MotiLink
-          href="/user/xxx"
-          animate={({ hovered, pressed }) => {
-            'worklet'
+        <View className="flex flex-row items-center justify-center">
+          <TouchableOpacity
+            onPress={() => alert('implement adding an activity next')}
+          >
+            <View className="mr-4 flex flex-row items-center justify-center rounded-full bg-gray-700 p-2 text-white">
+              <Plus width={24} height={24} fill="white" color={'white'} />
+              <Text className="ml-2 mr-2 text-xs font-bold text-white">
+                Add Activity
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <MotiLink
+            href="/user/xxx"
+            animate={({ hovered, pressed }) => {
+              'worklet'
 
-            return {
-              scale: pressed ? 0.95 : hovered ? 1.1 : 1,
-              rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
-            }
-          }}
-          transition={{
-            type: 'timing',
-            duration: 150,
-          }}
-          style={undefined}
-          onLayout={undefined}
-        >
-          <SolitoImage
-            alt="user-image"
-            src={require('../../../../assets/images/ulises.jpeg')}
-            style={{ borderRadius: 40, padding: 8 }}
-            height={50}
-            width={50}
-          />
-        </MotiLink>
+              return {
+                scale: pressed ? 0.95 : hovered ? 1.1 : 1,
+                rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
+              }
+            }}
+            transition={{
+              type: 'timing',
+              duration: 150,
+            }}
+            style={undefined}
+            onLayout={undefined}
+          >
+            <SolitoImage
+              alt="user-image"
+              src={require('../../../../assets/images/ulises.jpeg')}
+              style={{ borderRadius: 40, padding: 8 }}
+              height={50}
+              width={50}
+            />
+          </MotiLink>
+        </View>
       </View>
     </SafeAreaView>
   )
